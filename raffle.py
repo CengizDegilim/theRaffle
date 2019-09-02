f342b5831008890a04524dbcb3154987d629cb69
Ceyhun Gulbas <44509662+CengizDegilim@users.noreply.github.com>
2019-09-02 23:27:39 +0300

    Add files via upload

---
+++ raffle.py
@@ -0,0 +1,22 @@
+
+#    _   _            _____        __  __ _      
+#   | | | |          |  __ \      / _|/ _| |     
+#   | |_| |__   ___  | |__) |__ _| |_| |_| | ___ 
+#   | __| '_ \ / _ \ |  _  // _` |  _|  _| |/ _ \
+#   | |_| | | |  __/ | | \ \ (_| | | | | | |  __/
+#    \__|_| |_|\___| |_|  \_\__,_|_| |_| |_|\___|
+#                                                
+#                                                
+# made by: Ceyhun Gülbaş
+# https://github.com/CengizDegilim
+
+import random
+
+print("<<<<<<<<<<<<<<<<<<<<<<<<<<\n"+"|\t\t\t|"+"\n|Welcome to the Raffle. |"+"\n|\t\t\t|\n"+">>>>>>>>>>>>>>>>>>>>>>>>>")
+
+size = int(input('\n\n\nHow many participants applied: '))
+values = [(input('Names of participants: ')) for _ in range(size)]
+print("List of participants: ", ', '.join(values))
+
+winner = random.sample(values, 1)
+print("Winner: ", ', '.join(winner))
